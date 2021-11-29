d1fedcda86d79611a6f980de1cf4f57fdc53ee5a
Quentin <q_cozic@hotmail.fr>
2021-11-29 16:45:06 +0100

    m

---
+++ public/js/background.js
@@ -0,0 +1,57 @@
+const backg = document.querySelector('.backgroundHeader')
+const link = document.querySelectorAll('header ul li')
+let speed = 500
+let intervale1
+
+function createBubulle(){
+
+    let bubulle = document.createElement('div')
+    bubulle.classList.add('bubulle')
+
+    let widthHeight = Math.floor(Math.random()*100 + 50)
+    let numberLeft = Math.floor(Math.random()*100)
+    let numberColor = Math.floor(Math.random()*360)
+
+    bubulle.style.cssText += "left: " + numberLeft + "%; height:" + widthHeight + "px; width: " + widthHeight + "px; background-image: radial-gradient(farthest-corner at 25px 25px, hsl(" + numberColor + ", 10%, 50%) 0%, hsl(" + numberColor + ", 10%, 30%) 100%);"
+    setTimeout(()=>{
+        bubulle.style.cssText += "transform: translateY(700px); transition: transform 20s;"
+    },100)
+    backg.appendChild(bubulle)
+    setTimeout(() => {
+        bubulle.remove()
+    }, 10000);
+}
+
+setInterval(() => {
+    createBubulle()
+}, 200);
+
+function createA(element){
+    let bulle = document.createElement('div')
+    bulle.classList.add('bulle')
+
+    let numberLeft = Math.floor(Math.random()*30 + 35)
+    let numberColor = Math.floor(Math.random()*360)
+
+    bulle.style.cssText += "opacity: 1; left: " + numberLeft + "%; background-image: radial-gradient(farthest-corner at 5px 10px, hsl(" + numberColor + ", 100%, 50%) 0%, hsl(" + numberColor + ", 100%, 30%) 100%);"
+    element.appendChild(bulle)
+    setTimeout(()=>{
+         let numberX = Math.floor(Math.random()*150 - 75)
+         bulle.style.cssText += "opacity: 0; transform: translateY(-100px) translateX(" + numberX + "px); transition: all 2s;"
+    },100)
+    setTimeout(() => {
+        bulle.remove()
+    }, 1000);
+}
+link.forEach(element => {
+    element.addEventListener('mouseover', function(){
+        intervale1 = setInterval(()=>{
+            createA(element)
+        }, 200)
+    })
+});
+link.forEach(element => {
+element.addEventListener('mouseout', function(){
+    clearInterval(intervale1)
+})
+});
--- view/header.php
+++ view/header.php
@@ -1,10 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../public/css/acceuil.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&family=Poppins:wght@100;200;300;400;500;600;700&family=Zen+Kurenaido&display=swap" rel="stylesheet">
+</head>
+<body>
 
+<div class="backgroundHeader">
+        <h1>Concepteur Application</h1>
+        <h3>Lorem, ipsum dolor.</h3>
+</div>
 <header>
+    <div class="logo">
+
+    </div>
     <nav>
-        <li><a href="../view/index.php">Acceuil</a></li>
-        <li><a href="../view/toDo.php">ToDo Liste</a></li>
+        <ul>
+            <li><a href="">Acceuil</a></li>
+            <li><a href="">ToDo List</a></li>
+            <li><a href="">Galerie Image</a></li>
+        </ul>
     </nav>
-    <div class="pseudo">
-        <?php if(isset($_SESSION['name'])){echo "Bonjour " . $_SESSION['name'];}?>
+    <div class="login">
+        <button class="loginConnexion">Se Connecter</button>
     </div>
 </header>
+
+<script src="../public/js/background.js"></script>
